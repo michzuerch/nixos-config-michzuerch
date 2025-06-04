84cfdf6e53a07e4df36bbd331343a722bfc30ed6
Michael Zürcher <michzuerch@gmail.com>
2025-06-04 08:43:43 +0000

    Update default.nix

--- hosts/modules/desktop/default.nix
+++ hosts/modules/desktop/default.nix
@@ -1,13 +1,13 @@
 {
   imports = [
-    ./cinnamon.nix
-    ./cosmic.nix
+    #./cinnamon.nix
+    #./cosmic.nix
     # ./gnome.nix Build broken May 30 2025
     ./hyprland.nix
-    ./mate.nix
+    #./mate.nix
     # Cant use patheon together with gnome
     # ./pantheon.nix
-    ./plasma6.nix
+    #./plasma6.nix
     ./xfce.nix
   ];
 }

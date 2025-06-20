0f08c002ec964e0fe41284570a478285b143056b
Michael Zürcher <michzuerch@gmail.com>
2025-06-20 16:17:51 +0200

    Update disko.nix
    Decribe disk by id

--- hosts/ElitebookNomad/disko.nix
+++ hosts/ElitebookNomad/disko.nix
@@ -2,7 +2,7 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/sdb";
+        device = "/dev/disk/by-id/ata-SanDisk_SD8SN8U-512G-1006_162686420836";
         type = "disk";
         content = {
           type = "gpt";

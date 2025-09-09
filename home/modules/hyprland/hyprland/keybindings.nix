1a3fb74ef95d145db4cb9e4af26021174d553289
Michael Zuercher <michzuerch@gmail.com>
2025-09-09 12:28:39 +0200

    fix keybinding for swayosd in hyprland

--- home/modules/hyprland/hyprland/keybindings.nix
+++ home/modules/hyprland/hyprland/keybindings.nix
@@ -89,14 +89,18 @@
     ];
 
     bindel = [
-      ",XF86AudioRaiseVolume,  exec swayosd-client --output-volume raise"
-      ",XF86AudioLowerVolume,  exec swayosd-client --output-volume lower"
-      ",XF86AudioMute,         exec swayosd-client --output-volume mute-toggle"
+      ",XF86AudioRaiseVolume,   exec, swayosd-client --output-volume raise"
+      ",XF86AudioLowerVolume,   exec, swayosd-client --output-volume lower"
+      ",XF86AudioMute,          exec, swayosd-client --output-volume mute-toggle"
+      ",XF86AudioMicMute,       exec, swayosd-client --input-volume mute-toggle"
 
       #",XF86AudioRaiseVolume,  exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
       #",XF86AudioLowerVolume,  exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
       #",XF86AudioMute,         exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       #",XF86AudioMicMute,      exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+      ",XF86MonBrightnessUp,    exec, swayosd-client --brightness raise"
+      ",XF86MonBrightnessDown,  exec, swayosd-client --brightness lower"
+
       ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
       ",XF86MonBrightnessDown,  exec, brightnessctl s 10%-"
       ",switch:on:Lid Switch, exec, hyprctl dispatch dpms off"

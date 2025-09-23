faf13684c448d8274c9ba136681f8f6c0693a922
Michael Zürcher <michzuerch@gmail.com>
2025-09-23 16:38:28 +0200

    Update sops.nix

--- hosts/modules/sops.nix
+++ hosts/modules/sops.nix
@@ -3,7 +3,7 @@
     defaultSopsFile = ./secrets.yaml;
     defaultSopsFormat = "yaml";
 
-    age.keyFile = "/root/keys.txt";
+    #age.keyFile = "/root/keys.txt";
 
     secrets.github_api_key = {
       owner = config.users.users.michzuerch.name;

8bdbf6449d04530be6e423a4a797530b9f033767
Michael Zuercher <michzuerch@gmail.com>
2025-12-01 14:12:43 +0100

    new gtk for stylix

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -4,33 +4,7 @@
 
     gtk2.enable = false;
 
-    font = {
-      package = pkgs.noto-fonts;
-      name = "NotoSans";
-    };
-
-    cursorTheme = {
-      #   name = "Catppuccin-Macchiato-Blue";
-      #   package = pkgs.catppuccin-cursors.macchiatoBlue;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Original-Ice";
-      size = 24;
-    };
-
-    theme = {
-      #   name = "Juno-ocean";
-      #   package = pkgs.juno-theme;
-      name = "Catppuccin-Macchiato-Compact-Blue-dark";
-      package = pkgs.catppuccin-gtk.override {
-        size = "compact";
-        accents = ["blue"];
-        variant = "macchiato";
-      };
-    };
-
     iconTheme = {
-      #   name = "BeautyLine";
-      #   package = pkgs.beauty-line-icon-theme;
       name = "Papirus-Dark";
       package = pkgs.papirus-folders;
     };

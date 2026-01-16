013cab125ac231ccea2ea62580a278473a5ef5d8
Michael Zuercher <michzuerch@gmail.com>
2026-01-16 03:47:38 +0100

    discord with vencord

--- home/modules/discord.nix
+++ home/modules/discord.nix
@@ -3,6 +3,10 @@
 
   programs.nixcord = {
     enable = true;
+    notifyAboutUpdates = true;
+    discord.enable = false;
+    vesktop.enable = true;
+    equibop.enable = true;
     config = {frameless = true;};
   };
 }
